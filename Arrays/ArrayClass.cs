253ea24ef2c349da5eeef298d01313ad83246846
terryzeng <terryzeng98@gmail.com>
2016-03-30 10:59:45 -0400

    add ArrayClass.cs

---
+++ Arrays/ArrayClass.cs
@@ -0,0 +1,40 @@
+using System;
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] list = {34, 72, 13, 44, 25, 30, 10};
+            int[] temp = list;
+            Console.Write("Original Array: ");
+
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            //reverse the array
+            Array.Reverse(temp);
+            Console.Write("Reversed Array: ");
+
+            foreach (int i in temp)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            //sort the array
+            Array.Sort(list);
+            Console.Write("Sorted Array: ");
+
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
